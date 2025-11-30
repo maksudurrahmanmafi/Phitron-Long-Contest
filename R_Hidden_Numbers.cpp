bed09cfa83c39dc4eb9467a2c83b8163fd6d405a
Mafi2482 <maksudurrahmanmafi0@gmail.com>
2025-11-30 12:58:56 +0600

    Hidden Number

---
+++ R_Hidden_Numbers.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+     
+        int a =1,b=n;
+        cout<<a<<" "<<n<<'\n';
+    }
+    
+    return 0;
+}
