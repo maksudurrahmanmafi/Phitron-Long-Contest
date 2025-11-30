be3c2c241a04402c7023b8f5b21bfc02f4db81d5
Mafi2482 <maksudurrahmanmafi0@gmail.com>
2025-11-30 17:05:22 +0600

    Two Knight

--- AG_Two_Knights.cpp
+++ AG_Two_Knights.cpp
@@ -1,16 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main() {
+#define ll long long
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    long long n;
-    if(!(cin >> n)) return 0;
-    for(long long k = 1; k <= n; ++k) {
-        long long cells = k * k;
-        long long total_pairs = cells * (cells - 1) / 2;
-        long long attacks = 0;
-        if(k >= 3) attacks = 4 * (k - 1) * (k - 2);
-        long long ans = total_pairs - attacks;
+    ll n;
+    cin >> n;
+    for (ll k = 1; k <= n; ++k)
+    {
+        ll cells = k * k;
+        ll sum = cells * (cells - 1) / 2;
+        ll attack = 0;
+        if (k >= 3)
+            attack = 4 * (k - 1) * (k - 2);
+        ll ans = sum - attack;
         cout << ans << '\n';
     }
     return 0;
